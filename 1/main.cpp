2a2b4dd4efcb1575f358a79816aba9ac1231c67b
SirWojtek <momatoku@gmail.com>
2015-07-08 20:01:03 +0200

    SprWiek

--- 1/main.cpp
+++ 1/main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 
+// Pobierz od uzytkownika wiek. W zaleznosci czy
+// jest pelnoletni wydrukuj odpowiedni tekst.
+
 using namespace std;
 
 int main()
 {
-    cout << "Hello World" <<endl;
+    int wiek;
+    cout << "Witaj! Podaj swoj wiek." <<endl;
+    cin >> wiek;
+    if (wiek<18){
+        cout << "Jestes niepelnoletni. You shall not pass."<<endl;
+    }
+    else{
+        cout << "Jestes pelnoletni."<<endl;
+    }
 
     return 0;
 }
